29621315a7e85c7fbdf1227bf4757879c15c66f0
fmixson <58919440+fmixson@users.noreply.github.com>
2021-08-08 14:26:01 -0700

    Add files via upload

---
+++ AED.py
@@ -0,0 +1,134 @@
+import selenium
+import re
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import requests
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import re
+import pandas as pd
+driver = webdriver.Chrome("C:/Users/family/PycharmProjects/chromedriver.exe")
+driver.get('https://secure.cerritos.edu/rosters/login.cgi')
+login = driver.find_element_by_name('login')
+login.send_keys('gvasquez')
+login = driver.find_element_by_name('passwd')
+login.send_keys('Celestino80!')
+button = driver.find_element_by_xpath('//*[@id="login_form"]/table/tbody/tr[3]/td[2]/input').click()
+#This waits for list of courses to load
+element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'clform')))
+
+headers = ['NO', 'Name', 'ID', 'Grade', 'Hours', 'Other', 'Section', 'Course']
+df = pd.DataFrame(columns=headers)
+pd.set_option('display.max_columns', None)
+
+def select_session(y):
+    drop_down = driver.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[2]/form[1]/p/select/option['+str(y)+']').click()
+
+def rosters(start,finish):
+    for i in range(start, finish):
+        # This clicks on radio button to open roster
+        counted_course = driver.find_element_by_xpath('//*[@id="clform"]/table/tbody/tr['+str(i)+']/td[2]').text
+        # print(counted_course)
+        if (counted_course != "AED 40.01") and ("IWAP" not in counted_course):
+            pass
+            # else:
+
+        else:
+            # print(counted_course)
+            # button2 = driver.find_element_by_xpath('//*[@id="clform"]/table/tbody/tr[109]/td[1]/input').click()
+            button2 = driver.find_element_by_xpath('//*[@id="clform"]/table/tbody/tr['+str(i)+']/td[1]/input').click()
+            # This waits until roster open to run
+            element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'college')))
+            # This clicks on final grades link
+            course = driver.find_element_by_xpath('/html/body/table/tbody/tr/td[2]/table[2]/tbody/tr[2]/td[3]').text
+            # print(course)
+            cutup = course.split()
+            # print(cutup)
+            for word in cutup:
+                if "(" in word:
+                    section = word
+            # section = cutup[4]
+                    section = section[1:]
+            # print(section)
+            final_hours = driver.find_element_by_xpath('//*[@id="FinalGrades_form"]/a').click()
+            course = driver.find_element_by_xpath('/html/body/table/tbody/tr/td[2]/table[2]/tbody/tr[2]/td[3]').text
+            # print(f'course{course}')
+            # This waits until syllabus upload page loads
+            course2 = cutup[0] + ' ' + cutup[1][:5]
+
+            element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'Roster_form')))
+            page_source = driver.page_source
+            soup = BeautifulSoup(page_source, 'lxml')
+
+            table = soup.find('table', {'bgcolor': 'white', 'cellpadding': '5', 'cellspacing': '0'})
+            try:
+                table2 = table.find_all('th')
+
+            except:
+                # print('exception')
+                driver.back()
+                driver.back()
+                continue
+            # print(f'table2{table2}')
+
+
+            table3 = table.find_all('tr')[1:]
+            # print(table3)
+            for j in table3:
+                raw_data = j.find_all('td')
+                row = [tr.text for tr in raw_data]
+                row.append(section)
+                row.append(course2)
+                # print(row)
+                try:
+                    length = len(df)
+                    df.loc[length] = row
+                except:
+                    continue
+
+
+            driver.back()
+            driver.back()
+        print(df)
+        df.to_csv('E:/Work/Division/IWPA_Raw_Data' + str(z) +'.csv')
+        section_list = []
+        for i in range(len(df)):
+            if df.loc[i,'Section'] not in section_list:
+                section_list.append(df.loc[i, 'Section'])
+
+        headers2 = ['Course', 'Section', 'Total']
+        # print(section_list)
+        df_sections = pd.DataFrame(columns=headers2)
+        # section_hours = []
+        for section in section_list:
+            total_hours = 0
+            section_hours = []
+            for i in range(len(df)):
+                if section == df.loc[i,'Section']:
+                    course3 = df.loc[i, 'Course']
+                    hours = df.loc[i,'Hours']
+                    print(hours)
+                    hours = hours.split()
+                    print(hours)
+                    hours = hours[0]
+                    print(hours)
+                    hours = float(hours)
+                    print(hours)
+                    total_hours = total_hours + hours
+            section_hours.append(course3)
+            section_hours.append(section)
+            section_hours.append(total_hours)# section_hours[section] = total_hours
+            # print(section_hours)
+            length = len(df_sections)
+            df_sections.loc[length] = section_hours
+    print(df_sections)
+    df_sections.to_csv('E:/Work/Division/IWPA_Section_Totals' + str(z) +'.csv')
+z = 1
+select_session(4)
+rosters(1, 100)
+select_session(3)
+rosters(1, 100)
+# z = z + 1
+# rosters(316,716)
---
+++ main.py
@@ -0,0 +1,91 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import requests
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import re
+import pandas as pd
+driver = webdriver.Chrome("C:/Users/family/PycharmProjects/chromedriver.exe")
+driver.get('https://secure.cerritos.edu/rosters/login.cgi')
+login = driver.find_element_by_name('login')
+login.send_keys('fmixson')
+login = driver.find_element_by_name('passwd')
+login.send_keys('Frankgrace129!')
+button = driver.find_element_by_xpath('//*[@id="login_form"]/table/tbody/tr[3]/td[2]/input').click()
+#This waits for list of courses to load
+element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'clform')))
+
+no_syllabus = []
+yes_syllabus = []
+instructors = []
+
+for i in range(110, 115):
+    # This clicks on radio button to open roster
+    button2 = driver.find_element_by_xpath('//*[@id="clform"]/table/tbody/tr['+str(i)+']/td[1]/input').click()
+    # This waits until roster open to run
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'college')))
+    instructor = driver.find_element_by_xpath('/html/body/table/tbody/tr/td[2]/table[2]/tbody/tr[1]/td[3]').text
+    course = driver.find_element_by_xpath('/html/body/table/tbody/tr/td[2]/table[2]/tbody/tr[2]/td[3]').text
+    # This clicks on syllabus to upload syllabus
+    syllabus = driver.find_element_by_xpath('//*[@id="Syllabus_form"]/a').click()
+    # This waits until syllabus upload page loads
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'DownloadRoster_form')))
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'lxml')
+    # print(soup.find('input', {'type': 'file'}))
+    check_for_syllabus = soup.find('input', {'type': 'file'})
+    # print(check_for_syllabus)
+    if check_for_syllabus == None:
+        yes_syllabus.append(course)
+    else:
+        no_syllabus.append(course)
+        instructors.append(instructor)
+    driver.back()
+    driver.back()
+# print(yes_syllabus)
+# print(no_syllabus)
+section = []
+column = ['Section']
+df = pd.DataFrame(columns=column)
+
+for course in no_syllabus:
+    i = 0
+    cutup = course.split()
+    print(cutup)
+    for word in cutup:
+        i = i + 1
+        print(i)
+        if word == "LEC)":
+            section = cutup[i-2]
+            print(section)
+            section1 = section[1:]
+            length = len(df)
+            df.loc[length] = section1
+print(df)
+df.to_csv('E:/Work/Division/Syllabi1.csv')
+
+
+
+
+    # uploaded = soup.find_all('syllabus1')
+    # print(uploaded)
+    # upload = driver.find_element_by_xpath('/html/body/table/tbody/tr/td[2]/form/div/table/tbody/tr[2]/td/input')
+    # print(course)
+    # if driv'/html/body/table/tbody/tr/td[2]/form/div/table/tbody/tr[2]/td/input' == "file":
+    # names = driver.find_element_by_name('syllabus1')
+    # print(names)
+    # if names == 'syllabus1':
+    #     no_syllabus.append(course)
+    #     print(no_syllabus)
+    # else:
+    #     yes_syllabus.append(course)
+
+# button2 = driver.find_element_by_xpath('//*[@id="clform"]/table/tbody/tr[1]/td[1]/input').click()
+
+# url = 'https://secure.cerritos.edu/rosters/class_list.cgi'
+# page = requests.get(url)
+# list_of_courses = BeautifulSoup(page.text,'lxml')
+# print(list_of_courses)
